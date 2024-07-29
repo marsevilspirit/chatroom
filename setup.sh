9edb0c553d56b4ac66cc3a26335d90567abfc808
marsevilspirit <marsevilspirit@gmail.com>
2024-07-29 10:56:57 +0800

    add setup.sh for quick start

---
+++ setup.sh
@@ -0,0 +1,16 @@
+if [ -d "build" ]; then
+    echo "Build directory exists"
+    rm -rf build
+    echo "Build directory removed"
+    mkdir build
+    echo "Build directory created"
+else 
+    mkdir build
+    echo "Build directory created"
+fi
+
+cd build 
+
+cmake ..
+
+make
